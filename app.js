cb8908ba7c6a51d533234ad4eebce79fa0c40635
Mike Kistler <mikekistler@microsoft.com>
2023-11-12 15:23:36 -0600

    Working

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+import fs from "fs";
 import glob from "glob";
 import path from "path";
 
@@ -12,35 +13,34 @@ program
   .name("check-breaking-changes")
   .description("Check for breaking changes")
   .argument("basePath", "Path to the base (stable) version")
-  .argument("newPath", "Path to the new version to check")
-  .option("-o, --outputFolder <dir>");
+  .argument("newPath", "Path to the new version to check");
 
 program.parse();
 const options = program.opts();
 
-// Automatically track and cleanup files at exit
-// temp.track();
-
-// List all the *.json files in the base folder
 const baseFiles = glob.sync(path.join(program.args[0], "*.json"));
-
 const newFiles = glob.sync(path.join(program.args[1], "*.json"));
 
-// Print the list of files
-//console.log("Base files: ", baseFiles);
+const fullDiff = [];
 
 // For each new file, check if it exists in the base folder, and if so, compare the two using openapi-diff
 for (const newFile of newFiles) {
   const newFileName = path.basename(newFile);
   const baseFile = path.join(program.args[0], newFileName);
   if (baseFiles.includes(baseFile)) {
-    //console.log("Comparing ", baseFile, " with ", newFile);
     const diff = await openApiDiff(baseFile, newFile);
     if (diff) {
-      console.log(diff);
+      fullDiff.push(...diff);
+      // for testing
+      //break;
     }
   }
 }
+
+// Write the output to a file
+const outputFileName = "breaking-changes.json";
+const output = JSON.stringify(fullDiff,null, 2);
+fs.writeFileSync(outputFileName, output);
 
 /**
  * Compares old and new specifications for breaking change detection.
@@ -50,7 +50,7 @@ for (const newFile of newFiles) {
  * @param newSpec Path to the new swagger specification file.
  */
 async function openApiDiff(oldSpec, newSpec) {
-  const oadOut = await oad.compare(oldSpec, newSpec, { consoleLogLevel: "warn" });
+  const oadOut = await oad.compare(oldSpec, newSpec, { consoleLogLevel: "off" });
 
   // fix up output from OAD, it does not output valid JSON
   const result = oadOut.replace(/}\s+{/gi, "},{");
